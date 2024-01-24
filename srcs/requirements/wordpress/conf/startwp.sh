481a2a2b789ad855bf718bc144f0834ea8a59aae
Victor Vasconcellos <VictorVasconcellos42@gmail.com>
2024-01-24 15:18:00 -0300

    config wp script done

---
+++ srcs/requirements/wordpress/conf/startwp.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+if [ ! -f "/var/www/wp-config.php" ]; then
+	echo "nao foi";
+	wp core download
+	wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=mariadb 
+	wp core install --url=vde-vasc.42.fr --title=Inception --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL}
+	wp user create ${WP_USER}  ${WP_USER_EMAIL}
+fi
+echo "foi";
+/usr/sbin/php-fpm82 -F
